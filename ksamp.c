3430b54397a7be29310b3d719f7f74b174202c16
goltango <goltango@gmail.com>
2013-09-06 02:29:38 -0300

    version mejorada de ksamp.c, con la Parte C en un 90% codificada. Falta corregir el System init Time.

--- ksamp.c
+++ ksamp.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <getopt.h>
 #include <string.h>
+#include <time.h>
 #define BUFFSIZE 100
 
 void validsOpt();
 void defaultInfo();
 void showCpuinfo();
 void showVersion();
-void showUptime();
+int showUptime();
 void showStats();
 void showMeminfo();
 void infoEstadistica();
 void parser(char txt[BUFFSIZE], char x);
+void initSystemDate();
 
 int main(int argc, char *argv[]) {
 	int opcion;
@@ -20,7 +22,6 @@ int main(int argc, char *argv[]) {
 
     if (argc == 1){
 		defaultInfo();
-
 	}
 
 	 while ((opcion = getopt (argc, argv, "sl:")) != -1){
@@ -29,6 +30,10 @@ int main(int argc, char *argv[]) {
 				if(argc == 2){
 					defaultInfo();
 					showStats();
+					initSystemDate();
+
+
+
                     showMeminfo();
 				}else{
 					validsOpt();
@@ -128,7 +133,7 @@ void showVersion(){
 * This file contains two numbers: the uptime of the system (seconds),
 * and the amount of time spent in idle process (seconds).
 */
-void showUptime(){
+int showUptime(){
     FILE *fd;
 	int time=0, days=0, hours=0, mins=0, secs=0;
 
@@ -147,6 +152,7 @@ void showUptime(){
 
 	//Filtra uptime y lo imprime
 	printf("Uptime: %dd:%dh:%dm:%ds\n",days,hours,mins,secs);
+	return time;
 }
 
 /**
@@ -163,6 +169,8 @@ void showStats(){
 	char str3 [20];
 	char str4 [20];
 	char str5 [20];
+	char str6 [20];
+    int flag1 = 1, flag2 = 1;
 
     fd = fopen("/proc/stat","r");
 	fgets(stat1, BUFFSIZE+1, fd);
@@ -170,9 +178,17 @@ void showStats(){
 	//La cantidad de cpu puede variar, por ende no es seguro hacer fgets un numero fijo de veces
 	//Por eso el while ejecuta fgets() hasta encontrar la primer linea que empiece con 'p',
 	//que es la linea que contiene el valor buscado.
-	while(str4[0] != 'p'){
+	while(!feof(fd)){
         fgets(stat2, BUFFSIZE+1, fd);
-        sscanf(stat2,"%s %s", str4, str5);
+
+        if(strstr(stat2, "ctxt ") && flag1){
+            sscanf(stat2,"%*s %s", str6);
+            flag1 = 0;
+        }
+        if(strstr(stat2, "processes ") && flag2){
+            sscanf(stat2,"%*s %s", str5);
+            flag2 = 0;
+        }
 	}
 	fclose(fd);
 	fflush(fd);
@@ -185,6 +201,8 @@ void showStats(){
 
     //Filtra el numero de procesos creados y los imprime
     printf("Processes created: %s\n", str5);
+
+    printf("Context Changes: %s\n", str6);
 
 }
 /**
@@ -219,7 +237,19 @@ void showMeminfo(){
 }
 
 /**
- * Retorna parte de una cadena despues de un determinado caracter x
+* Muestra la fecha y hora de inicio del sistema.
+*/
+void initSystemDate(){
+    time_t mytime;
+
+    mytime = time(NULL);
+
+    printf("System init time: %s", ctime(&mytime));
+
+
+}
+/**
+ * Modifica parte de una cadena despues de un determinado caracter x
  * @param txt[BUFF] cadena a modificar
  * @param x caracter de limite
  */
